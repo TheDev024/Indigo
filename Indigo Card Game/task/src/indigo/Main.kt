a829a798a8486ac1b1b15928d29c81389afdff57
Abdullah Mahmudov <abdulla.mahmudov@nspsolutions.net>
2022-12-03 00:30:16 +0400

    game logic stage-4

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -3,6 +3,48 @@ package indigo
 import java.util.*
 
 val scanner = Scanner(System.`in`)
+val random = Random(24)
+
+open class Player(
+    open val name: String, open val hand: MutableList<Card>, open val wonCards: Stack<Card>, open var score: Int
+) {
+    open fun play(): Card? = null
+}
+
+data class User(
+    override val name: String = "Player",
+    override val hand: MutableList<Card> = mutableListOf(),
+    override val wonCards: Stack<Card> = Stack(),
+    override var score: Int = 0
+) : Player(name, hand, wonCards, score) {
+    override fun play(): Card? {
+        println("Cards in hand: ${hand.joinToString(" ") { "${hand.indexOf(it) + 1})$it" }}")
+        val input = chooseCard()
+        if (input == "exit") return super.play()
+        val index = input.toInt() - 1
+        return hand.removeAt(index)
+    }
+
+    private fun chooseCard(): String {
+        println("Choose a card to play (1-${hand.size}):")
+        val input = scanner.nextLine()
+        return if (input == "exit" || (input.toIntOrNull() ?: 0) in 1..hand.size) input
+        else chooseCard()
+    }
+}
+
+data class Computer(
+    override val name: String = "Computer",
+    override val hand: MutableList<Card> = mutableListOf(),
+    override val wonCards: Stack<Card> = Stack(),
+    override var score: Int = 0
+) : Player(name, hand, wonCards, score) {
+    override fun play(): Card {
+        val index = random.nextInt(hand.size)
+        println("Computer plays ${hand[index]}")
+        return hand.removeAt(index)
+    }
+}
 
 enum class Suit(val value: Char) {
     DIAMOND('\u2666'), HEART('\u2665'), SPADE('\u2660'), CLUB('\u2663'),
@@ -15,71 +57,121 @@ enum class Rank(val value: String) {
     KING("K")
 }
 
-data class Card(
-    val rank: Rank, val suit: Suit
-) {
-    val cardValue = "${rank.value}${suit.value}"
+data class Card(val rank: Rank, val suit: Suit) {
+    override fun toString(): String = rank.value + suit.value
 }
 
 class IndigoCardGame {
-    private val deck: List<Card>
-    private val cards = mutableListOf<Card>()
+
+    private val cards: List<Card>
+
+    private val deck: Stack<Card> = Stack()
+    private val table: Stack<Card> = Stack()
+
+    private lateinit var currentPlayer: Player
+    private lateinit var lastWinner: Player
+
+    private val user = User()
+    private val computer = Computer()
 
     init {
-        Suit.values().forEach { suit ->
-            Rank.values().forEach { rank ->
-                cards.add(Card(rank, suit))
+        Rank.values().forEach { rank ->
+            Suit.values().forEach { suit ->
+                deck.add(
+                    Card(rank, suit)
+                )
             }
         }
-        this.deck = cards.toList()
+        cards = deck.toList()
+        deck.shuffle()
     }
 
-    fun cmd() {
+    fun startGame() {
+        println("Indigo Card Game")
+
+        val firstPlayer = firstPlayer()
+        currentPlayer = firstPlayer
+        lastWinner = currentPlayer
+
+        print("Initial cards on the table:")
+        repeat(4) { print(" ${table.push(deck.pop())}") }
+
         while (true) {
-            println("Choose an action (reset, shuffle, get, exit):")
-            when (scanner.nextLine()) {
-                "reset" -> resetDeck()
+            if (user.wonCards.size + computer.wonCards.size + table.size == 52) {
+                println(if (table.empty()) "No cards on the table" else "\n${table.size} cards on the table, and the top card is ${table.peek()}")
+                if (!table.empty()) {
+                    lastWinner.wonCards.addAll(table)
+                    repeat(table.size) {
+                        if (table.pop().rank in listOf(
+                                Rank.ACE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING
+                            )
+                        ) lastWinner.score++
+                    }
 
-                "shuffle" -> {
-                    cards.shuffle()
-                    println("Card deck is shuffled.")
+                    if (user.wonCards.size > computer.wonCards.size) user.score += 3
+                    else if (user.wonCards.size < computer.wonCards.size) computer.score += 3
+                    else firstPlayer.score += 3
+
+                    printState()
                 }
-
-                "get" -> getCards()
-
-                "exit" -> break
-
-                else -> println("Wrong action.")
+                break
             }
+
+            if (computer.hand.size == 0 && user.hand.size == 0) {
+                dealPlayerCards(computer)
+                dealPlayerCards(user)
+            }
+
+            println(if (table.empty()) "No cards on the table" else "\n${table.size} cards on the table, and the top card is ${table.peek()}")
+            val card = currentPlayer.play() ?: break
+
+            if (!table.empty()) {
+                val topCard = table.peek()
+                table.push(card)
+                if (card.rank == topCard.rank || card.suit == topCard.suit) {
+                    currentPlayer.wonCards.addAll(table)
+                    repeat(table.size) {
+                        if (table.pop().rank in listOf(
+                                Rank.ACE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING
+                            )
+                        ) currentPlayer.score++
+                    }
+                    println("${currentPlayer.name} wins cards")
+                    printState()
+                    lastWinner = currentPlayer
+                }
+            } else table.push(card)
+            changeTurn()
         }
-        println("Bye")
+        println("Game Over")
     }
 
-    private fun getCards() {
-        println("Number of cards:")
-        val n = scanner.nextLine()
-        if (Regex("\\d+").matches(n) && n.toInt() in 1..52) {
-            val number = n.toInt()
-            if (number > cards.size) println("The remaining cards are insufficient to meet the request.") else {
-                val selectedCards = cards.subList(0, number).toList()
-                cards.removeAll(selectedCards)
-                println(selectedCards.joinToString(" ") { it.cardValue })
-            }
-        } else println("Invalid number of cards.")
+    private fun printState() {
+        println("Score: ${user.name} ${user.score} - ${computer.name} ${computer.score}")
+        println("Cards: ${user.name} ${user.wonCards.size} - ${computer.name} ${computer.wonCards.size}")
     }
 
-    private fun resetDeck() {
-        cards.clear()
-        Suit.values().forEach { suit ->
-            Rank.values().forEach { rank ->
-                cards.add(Card(rank, suit))
-            }
+    private fun changeTurn() {
+        currentPlayer = if (currentPlayer == user) computer else user
+    }
+
+    private fun firstPlayer(): Player {
+        println("Play first?")
+        return when (scanner.nextLine().lowercase()) {
+            "yes" -> user
+
+            "no" -> computer
+
+            else -> firstPlayer()
         }
-        println("Card deck is reset.")
+    }
+
+    private fun dealPlayerCards(player: Player) {
+        repeat(6) { player.hand.add(deck.pop()) }
     }
 }
 
 fun main() {
     val game = IndigoCardGame()
-    game.cmd()
+    game.startGame()
 }
